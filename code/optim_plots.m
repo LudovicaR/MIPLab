37af8dc322d81cf1836ef786bc6489fcb83cf8f3
LudovicaR <ludovica.romanin@epfl.ch>
2020-06-10 16:25:07 +0200

    Update optim_plots.m

--- code/optim_plots.m
+++ code/optim_plots.m
@@ -17,8 +17,7 @@ load  optimizedhopfawake_56HC.mat;
 load empiricalLEiDA.mat;
 
 
-%%
-load HopfModel_results_56HC.mat FC_simul FCemp FCphasesemp;
+%% find optimal G and corresponding PMS
 
 % Optimal G for KL
 [M,I] = min(klpstates);
@@ -59,46 +58,3 @@ colorbar
 title('lesioned SC (w/o CC)')
 hold on
 
-%% FCD plots 
-load fcd_result_56HC_woCC.mat
-
-%%
-fcd_kl = phfcd_sim(I,:,:);
-fcd_ks = phfcd_sim(I1,:,:);
-
-fcd_kl = squeeze(fcd_kl);
-fcd_ks = squeeze(fcd_ks);
-
-%%
-N = 74;
-TP = 200;
-pat_size = N*(N-1) + (TP-21) - 2;
-fcd_length = pat_size*(pat_size-1)/2;
-
-fcd_length_area = floor(fcd_length/74);
-
-%%
-
-fcd_kl_areas = zeros(N,fcd_length_area);
-fcd_ks_areas = zeros(N,fcd_length_area);
-
-for seed=1:N
-    fcd_kl_areas(seed,:) = fcd_kl((seed-1)*fcd_length_area+1 : seed*fcd_length_area);
-    fcd_ks_areas(seed,:) = fcd_ks((seed-1)*fcd_length_area+1 : seed*fcd_length_area);
-end
-
-%%
-std_fcd_kl = std(fcd_kl_areas,0,2);
-std_fcd_ks = std(fcd_ks_areas,0,2);
-
-std_kl = std(fcd_kl);
-std_ks = std(fcd_ks);
-std_emp = std(phfcddata_emp);
-
-%%
-atlas_labels = readtable('dbs80symm_labels_NEW.txt');
-aal =  atlas_labels.Var2;
-%%
-x = linspace(1,74,74);
-std_bar = [std_fcd_kl, std_fcd_ks];
-b = bar(x,std_bar);
